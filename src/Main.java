bf0fef59aaf3e14c26f3bd8552d750ce1e0e048a
SPeterson97 <31021730+SPeterson97@users.noreply.github.com>
2019-04-05 12:47:28 -0400

    Binary Update
    Add to binary

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String [] args){
+        int i = 0;
+        for (;i<5;i++){
+            System.out.println(toBinary(i));
+        }
+    }
 
+    public static String toBinary(int num){
+        StringBuilder result = new StringBuilder();
+
+        while(num > 0) {
+            int r = num % 2;
+            num /= 2;
+            result.append(Integer.toString(r));
+        }
+        //System.out.println(result.reverse().toString());
+        return result.reverse().toString();
     }
 }
